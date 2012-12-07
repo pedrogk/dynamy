3ac16341ad712a1bb9918be5ffdafc07861f49cc
Eduardo Díaz <iamedu@gmail.com>
2012-12-07 01:10:23 -0600

    Creating logs

--- kernel/src/main/scala/ServerSetup.scala
+++ kernel/src/main/scala/ServerSetup.scala
@@ -5,6 +5,7 @@ import java.nio.file._
 class ServerSetup(val serverPath: String) {
 
   val confPath         = Paths.get(serverPath, "conf")
+  val logsPath         = Paths.get(serverPath, "logs")
   val kernelProperties = Paths.get(serverPath, "conf", "kernel.properties")
   val initializedPath  = Paths.get(serverPath, ".initialized")
 
@@ -12,6 +13,7 @@ class ServerSetup(val serverPath: String) {
 
 	def initialize() = if(!isInitialized) {
 		Files.createDirectories(confPath)
+		Files.createDirectories(logsPath)
 		Files.copy(getClass.getResourceAsStream("/kernel.properties"), kernelProperties)
 		Files.createFile(initializedPath)
 	}
