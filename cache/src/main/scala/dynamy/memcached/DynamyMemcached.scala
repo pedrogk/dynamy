b2add9f87683b0d6c663310f00e4eb16e2a1ecf4
Eduardo Díaz <iamedu@gmail.com>
2012-12-07 15:54:51 -0600

    Improving dynamy cache implementation

--- cache/src/main/scala/dynamy/memcached/DynamyMemcached.scala
+++ cache/src/main/scala/dynamy/memcached/DynamyMemcached.scala
@@ -1,18 +1,25 @@
 package dynamy.cache.xmemcached
 
 import dynamy.cache._
-
 import com.google.gson._
+
+import scala.collection.JavaConversions._
 
 import net.rubyeye.xmemcached._
 import net.rubyeye.xmemcached.command._
 import net.rubyeye.xmemcached.utils._
 
 class DynamyMemcache(val client: MemcachedClient) extends DynamyCache {
+  override def get[T](keys: Set[String]): Map[String, T] = {
+    val colls: java.util.Collection[String] = keys
+    val vals: java.util.Map[String, T] = client.get(colls)
+    vals.toMap
+  }
   override def get[T](key: String): T = client.get(key)
   override def get[T](key: String, timeout: Long): T = client.get(key, timeout)
   override def set[T](key: String, value: T): Unit = client.set(key, 0, value)
   override def set[T](key: String, exp: Int, value: T): Unit = client.set(key, exp, value)
+  override def set[T](key: String, exp: Int, value: T, timeout: Long): Unit = client.set(key, exp, value, timeout)
 }
 
 class DynamyMemcacheManager(val configurationFile: String) extends DynamyCacheService {
--- util/src/main/scala/dynamy/DynamyCache.scala
+++ util/src/main/scala/dynamy/DynamyCache.scala
@@ -6,9 +6,11 @@ trait DynamyCacheService {
 }
 
 trait DynamyCache {
+  def get[T](keys: Set[String]): Map[String, T]
   def get[T](key: String): T
   def get[T](key: String, timeout: Long): T
   def set[T](key: String, value: T): Unit
   def set[T](key: String, exp: Int, value: T): Unit
+  def set[T](key: String, exp: Int, value: T, timeout: Long): Unit
 }
 
