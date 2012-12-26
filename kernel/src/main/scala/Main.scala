bc73a48fb0f55776870fa2f15a14e6ed48775876
Eduardo Díaz <iamedu@gmail.com>
2012-12-26 09:45:19 -0600

    Creating dynamy install script

--- kernel/src/main/scala/Main.scala
+++ kernel/src/main/scala/Main.scala
@@ -5,36 +5,43 @@ import server._
 import java.util.logging._
 
 object Main {
-	val logger = Logger.getLogger("dynamy.Main")
+  val logger = Logger.getLogger("dynamy.Main")
 
-	val serverHome = System.getProperty("user.dir")
-    System.setProperty("prog.home", serverHome)
-	val serverSetup  = new ServerSetup(serverHome)
-	
+  val serverHome = System.getProperty("user.dir")
+  System.setProperty("prog.home", serverHome)
+  val serverSetup  = new ServerSetup(serverHome)
 
-	logger.info("Starting dynamy at %s".format(serverHome))
-	serverSetup.initialize()
 
-	val dynamyServer = new DynamyServer(serverHome)
+  logger.info("Starting dynamy at %s".format(serverHome))
+  serverSetup.initialize()
 
-	Runtime.getRuntime().addShutdownHook(new Thread {
-		override def run = {
-			logger.info("Finished service framework")
-			dynamyServer.stop
-			dynamyServer.waitForStop
-		}
-	})
-	
-	def main(args: Array[String]): Unit = {
-        import sun.misc._
-        val stopSignal = new java.util.concurrent.CountDownLatch(1)
-		val handler = new SignalHandler {
-            override def handle(sig: Signal) = {
-                stopSignal.countDown
-            }
+  val dynamyServer = new DynamyServer(serverHome)
+
+  Runtime.getRuntime().addShutdownHook(new Thread {
+    override def run = {
+      logger.info("Finished service framework")
+      dynamyServer.stop
+      dynamyServer.waitForStop
+    }
+  })
+
+  def main(args: Array[String]): Unit = {
+    if(args.length > 0 && args(0) == "install") {
+      logger.info("Installing dependencies") 
+      dynamyServer.install()
+      logger.info("Dependencies installed") 
+      System.exit(0)
+    } else {
+      import sun.misc._
+      val stopSignal = new java.util.concurrent.CountDownLatch(1)
+      val handler = new SignalHandler {
+        override def handle(sig: Signal) = {
+          stopSignal.countDown
         }
-        Signal.handle(new Signal("INT"), handler)
-		dynamyServer.start
-        stopSignal.await()
-	}
+      }
+      Signal.handle(new Signal("INT"), handler)
+      dynamyServer.start
+      stopSignal.await()
+    }
+  }
 }
--- kernel/src/main/scala/server/DynamyServer.scala
+++ kernel/src/main/scala/server/DynamyServer.scala
@@ -16,23 +16,23 @@ import java.nio.file._
 class DynamyServer(serverHome: String) {
 
   val logger = Logger.getLogger("dynamy.server.DynamyServer")
-  
+
   val frameworkFactory = ServiceLoader.load(classOf[FrameworkFactory]).iterator.next
-	val configuration = setup()
+  val configuration = setup()
   val framework = frameworkFactory.newFramework(configuration)
 
   def setup() = {
-  	var configuration = Map[String, String]()
-  	val kernelProperties = Paths.get(serverHome, "conf", "kernel.properties")
-  	val props = new Properties()
-  	props.load(new FileInputStream(kernelProperties.toFile))
-  	for((k, v) <- props) {
-  		configuration += (k.toString -> v.toString)
-        if(k.toString.startsWith("dynamy") || k.toString.startsWith("com.atomikos"))
-            System.setProperty(k.toString, v.toString)
-  	}
-  	configuration
-  }
+    var configuration = Map[String, String]()
+    val kernelProperties = Paths.get(serverHome, "conf", "kernel.properties")
+    val props = new Properties()
+    props.load(new FileInputStream(kernelProperties.toFile))
+    for((k, v) <- props) {
+      configuration += (k.toString -> v.toString)
+      if(k.toString.startsWith("dynamy") || k.toString.startsWith("com.atomikos"))
+        System.setProperty(k.toString, v.toString)
+}
+configuration
+    }
 
   def loadDrivers() = {
     import java.util.ServiceLoader
@@ -40,45 +40,54 @@ class DynamyServer(serverHome: String) {
     for(d <- drivers) {
       logger.info("Loading driver " + d)
       java.sql.DriverManager.registerDriver(d)
-    }
   }
+    }
 
   def installBundles() = {
-  	val bundles = io.Source.fromInputStream(getClass.getResourceAsStream("/initial.list")).getLines
-  	val Comment = """#(.*)""".r
-  	val Bundle  = """([^!]+)!?(start)?!?([1-9][0-9]*)?""".r
-  	for(b <- bundles) {
-  		b match {
-  			case Comment(c) => {
-  				logger.fine("Got a comment %s".format(c))
-  			}
-  			case Bundle(url, start, runlevel) => {
-                try {
-					val b = framework.getBundleContext.installBundle(url)
-					logger.info("Bundle %s installed".format(b))
-					if("start".equals(start)) {
-						b.start
-						logger.info("Bundle %s started".format(b))
-					}
-					if(runlevel != null) {
-						b.adapt(classOf[BundleStartLevel]).setStartLevel(runlevel.toInt)
-					}
-                } catch {
-                    case e => logger.log(Level.SEVERE, "Cannot download bundle " + b, e)
-                }
-  			}
-  			case _ => {}
-  		}
-  	}
+    val bundles = io.Source.fromInputStream(getClass.getResourceAsStream("/initial.list")).getLines
+    val Comment = """#(.*)""".r
+    val Bundle  = """([^!]+)!?(start)?!?([1-9][0-9]*)?""".r
+    for(b <- bundles) {
+      b match {
+        case Comment(c) => {
+          logger.fine("Got a comment %s".format(c))
+  }
+        case Bundle(url, start, runlevel) => {
+          try {
+            val b = framework.getBundleContext.installBundle(url)
+            logger.info("Bundle %s installed".format(b))
+            if("start".equals(start)) {
+              b.start
+              logger.info("Bundle %s started".format(b))
+        }
+        if(runlevel != null) {
+          b.adapt(classOf[BundleStartLevel]).setStartLevel(runlevel.toInt)
+            }
+          } catch {
+            case e => logger.log(Level.SEVERE, "Cannot download bundle " + b, e)
+          }
+          }
+            case _ => {}
+            }
+      }
+    }
+
+  def start = {
+    setup()
+    framework.start()
+    logger.info("Started osgi framework")
+    installBundles()
+    loadDrivers()
   }
 
-	def start = {
-		setup()
-		framework.start()
-		logger.info("Started osgi framework")
-		installBundles()
-        loadDrivers()
-	}
-	def stop  = if(framework != null) framework.stop
-	def waitForStop = framework.waitForStop(60 * 1000)
+  def install() = {
+    setup()
+    framework.start()
+    installBundles()
+    stop
+  }
+
+  def stop  = if(framework != null) framework.stop
+  
+  def waitForStop = framework.waitForStop(60 * 1000)
 }
