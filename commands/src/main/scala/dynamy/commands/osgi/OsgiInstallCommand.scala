faee4306a7aa4575d6f9d5b65e97ecd1035cced1
Eduardo Díaz <iamedu@gmail.com>
2012-12-27 01:58:09 -0600

    Testing commands

--- commands/src/main/scala/dynamy/commands/osgi/OsgiInstallCommand.scala
+++ commands/src/main/scala/dynamy/commands/osgi/OsgiInstallCommand.scala
@@ -6,6 +6,7 @@ import joptsimple.OptionParser
 import org.slf4j.LoggerFactory
 import org.osgi.framework.FrameworkUtil
 import dynamy.shell.annotations.DynamyCommandMeta
+import org.osgi.framework.startlevel._
 
 @DynamyCommandMeta(
   namespace = "osgi",
@@ -24,24 +25,36 @@ class OsgiInstallCommand extends DynamyCommand {
     val str = new StringBuffer
 
     for (arg <- argsOnly) {
+      val Bundle  = """([^!]+)!?(start)?!?([1-9][0-9]*)?""".r
       try {
-        val installed = bc.installBundle(arg)
-        str.append("Bundle ")
-        str.append(installed)
-        str.append(" installed from ")
-        str.append(arg)
+        arg match {
+          case Bundle(url, start, runlevel) => {
+            val installed = bc.installBundle(url)
+            if("start".equals(start)) {
+              b.start
+              logger.info("Bundle %s started".format(b))
+            }
+            if(runlevel != null) {
+              b.adapt(classOf[BundleStartLevel]).setStartLevel(runlevel.toInt)
+            }
+            str.append("Bundle ")
+            str.append(installed)
+            str.append(" installed from ")
+            str.append(arg)
+          }
+        }
       } catch {
         case e => {
-          str.append("Cannot install bundle ")
-          str.append(arg)
-          str.append(": ")
-          str.append(e.getMessage())
-          logger.error("Unable to install bundle", e)
+            str.append("Cannot install bundle ")
+            str.append(arg)
+            str.append(": ")
+            str.append(e.getMessage())
+            logger.error("Unable to install bundle", e)
         }
       }
       str.append("\n")
+        }
+        str.toString()
     }
-    str.toString()
-  }
 
-}
+  }
